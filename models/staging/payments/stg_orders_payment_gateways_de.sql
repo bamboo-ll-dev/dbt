f80135b9106312f83a52e774f727cdeaa7c09e4e
Sebastian <sebastian@leslunes.de>
2021-02-26 13:12:49 +0100

    transfer payment gateway models

--- models/staging/payments/stg_orders_payment_gateways_de.sql
+++ models/staging/payments/stg_orders_payment_gateways_de.sql
@@ -2,7 +2,10 @@
 SELECT id AS id2, payment_gateway_names FROM
 (SELECT row_number() OVER (PARTITION BY id ORDER BY updated_at DESC) AS rn, updated_at, id, payment_gateway_names FROM
 (SELECT updated_at, id, STRING_AGG(payment_gateway_names) AS payment_gateway_names FROM
-(SELECT updated_at, id, payment_gateway_names.value AS payment_gateway_names FROM leslunes-raw.shopify_fr.orders
+(SELECT updated_at, id, payment_gateway_names.value AS payment_gateway_names 
+
+FROM {{ source('shopify_de','orders') }}
+
 LEFT JOIN UNNEST(payment_gateway_names) AS payment_gateway_names 
 GROUP BY updated_at, id, payment_gateway_names) AS A
 GROUP BY updated_at, id) AS A
