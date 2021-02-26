7f2b1e6e15e601c050ed3a6c35aa34daec7152fb
Sebastian <sebastian@leslunes.de>
2021-02-26 11:03:11 +0100

    add model and source templates

---
+++ models/staging/abondoned_checkouts/stg_abondoned_checkouts_fr.sql
@@ -0,0 +1,14 @@
+SELECT * FROM(
+SELECT 
+  id 
+  ,customer_locale
+  ,email
+  ,abandoned_checkout_url
+  ,name 
+  ,updated_at 
+  ,created_at
+  , row_number() OVER(PARTITION BY id, email,name, created_at) as row_number
+FROM  {{ source('abondoned_checkouts_fr','abandoned_checkouts') }}
+
+)
+WHERE row_number = 1
