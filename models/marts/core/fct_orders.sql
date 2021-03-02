1284e51eaefc7cef8ce1ca38e83e6733d429461e
Sebastian <sebastian@leslunes.de>
2021-03-02 10:02:13 +0100

    add orders to facts

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,23 @@
+SELECT
+   id AS order_id
+  ,number
+  ,name AS shop_order_refrence
+  ,regexp_extract(name, r"[0-9].*") AS ordernumber
+  ,gateway
+  ,total_price
+  ,total_tax
+  ,tax_rate
+  ,financial_status
+  ,fulfillment_status
+  ,total_discounts
+  ,discount_codes
+  ,discount_value
+  ,discount_codes__type AS discount_codes_type
+  ,shippment_amount
+  ,filfillment_updated_at
+  ,status	
+  ,source
+  ,created_at	
+  ,updated_at
+FROM
+  {{ ref('stg_orders_combined') }}
