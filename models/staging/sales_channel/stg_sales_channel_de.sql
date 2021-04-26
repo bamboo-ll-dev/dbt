c5b68b216f6b0f88cafb4241cfc65c4a5a79624c
Sebastian Schult <sebastian@leslunes.de>
2021-04-26 08:38:32 +0000

    add sales channel coupon codes to staging

---
+++ models/staging/sales_channel/stg_sales_channel_de.sql
@@ -0,0 +1,21 @@
+SELECT 
+  transaction_id,
+  created_at,
+  o.code,
+  value,
+  fixedAmount,
+  type,
+  startsAt,
+  endsAt,
+  influencerfullName,
+  influencerUsernamesAsString,
+  follower,
+  totalRevenueNet,
+  appliedCount,
+  c.source,
+  db_entry_created_at,
+  code_type
+FROM {{ ref('orders_combined')}} o 
+LEFT JOIN {{ ref('coupons') }} c ON UPPER(c.code) = UPPER(o.code) --AND DATETIME(o.created_at) BETWEEN c.startsAt AND c.endsAt
+--WHERE UPPER(o.code) = "HIELENA40"
+AND endsAt is null
