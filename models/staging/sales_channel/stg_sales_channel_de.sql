b773932d61483f074c35fab3801a18d6d4812d8a
Sebastian Schult <sebastian@leslunes.de>
2021-04-26 08:39:18 +0000

    bugfix

--- models/staging/sales_channel/stg_sales_channel_de.sql
+++ models/staging/sales_channel/stg_sales_channel_de.sql
@@ -15,7 +15,7 @@ SELECT
   c.source,
   db_entry_created_at,
   code_type
-FROM {{ ref('orders_combined')}} o 
+FROM {{ ref('stg_orders_combined')}} o 
 LEFT JOIN {{ ref('coupons') }} c ON UPPER(c.code) = UPPER(o.code) --AND DATETIME(o.created_at) BETWEEN c.startsAt AND c.endsAt
 --WHERE UPPER(o.code) = "HIELENA40"
 AND endsAt is null
