abd1d0332cfdccdff551c778e56edbaa50e17dd6
Sebastian <sebastian@leslunes.de>
2021-06-21 14:18:04 +0200

    add discounts to order items

--- models/staging/orderitems/stg_order_items_fr.sql
+++ models/staging/orderitems/stg_order_items_fr.sql
@@ -1,5 +1,4 @@
 WITH line_items AS(
-#Line items
 SELECT row_number() OVER (PARTITION BY li.value.id, li.value.sku ORDER BY updated_at DESC) AS row_number, 
   li, 
   id,
@@ -7,9 +6,20 @@ SELECT row_number() OVER (PARTITION BY li.value.id, li.value.sku ORDER BY update
   o.note,
   o.number,
   o.order_number,
-  o.name
-  FROM  `leslunes-raw.shopify_fr.orders` o,  
-  UNNEST(line_items) AS li 
+  o.name,
+  
+  SAFE_CAST(dall.value.amount_set.shop_money.amount AS NUMERIC) AS item_discount,
+  
+  dapp.value.value_type AS coupon_value_type,	
+  dapp.value.target_type AS coupon_target_type,	 
+  dapp.value.type AS coupon_type,
+  dapp.value.value AS coupon_value,	
+  dapp.value.code AS coupon_code
+  
+ FROM  `leslunes-raw.shopify_fr.orders` o,  
+ UNNEST(line_items) AS li,
+ UNNEST(discount_applications) AS dapp,
+ UNNEST(li.value.discount_allocations) AS dall
 )
 
 
@@ -21,14 +31,19 @@ SELECT
   li.value.title AS shopify_product_title,
   li.value.variant_title AS shopify_variant_title ,	
   li.value.name AS shopify_product_name,
-
   li.value.price AS item_price_original,
   li.value.quantity AS ordered_quantity,
 
-  SAFE_CAST(da.value.amount_set.shop_money.amount AS NUMERIC) AS item_discount,
+  item_discount,
   tx.value.rate AS tax_rate,
   tx.value.price AS tax_amount,
   tx.value.title AS tax_type,
+  
+  coupon_value_type,	
+  coupon_target_type,	
+  coupon_type,
+  coupon_value,	
+  coupon_code,
 
   li.value.total_discount_set.shop_money.currency_code,
   li.value.taxable,
@@ -45,8 +60,6 @@ SELECT
   li.value.variant_id AS shopify_variant_id
 
 FROM line_items,
-UNNEST(li.value.tax_lines)As tx,
-UNNEST(li.value.discount_allocations) AS da
+UNNEST(li.value.tax_lines) AS tx
 WHERE row_number = 1
 ORDER BY id
-
