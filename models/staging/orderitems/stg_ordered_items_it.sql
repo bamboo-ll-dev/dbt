ab0ec9a0966bbd899ba9bd6b79550f11875aed3e
Sebastian <sebastian@leslunes.de>
2021-06-21 13:45:20 +0200

    update orderitems schemas

--- models/staging/orderitems/stg_ordered_items_it.sql
+++ models/staging/orderitems/stg_ordered_items_it.sql
@@ -1,67 +1,52 @@
+WITH line_items AS(
+#Line items
+SELECT row_number() OVER (PARTITION BY li.value.id, li.value.sku ORDER BY updated_at DESC) AS row_number, 
+  li, 
+  id,
+  o.created_at,
+  o.note,
+  o.number,
+  o.order_number,
+  o.name
+  FROM  `leslunes-raw.shopify_de.orders` o,  
+  UNNEST(line_items) AS li 
+)
+
+
 SELECT 
-*,
-#Additionally created columns
-#ROUND(SAFE_DIVIDE(SAFE_CAST(total_tax AS FLOAT64), (SAFE_CAST(total_line_items_price AS FLOAT64) - SAFE_CAST(total_tax AS FLOAT64)))) AS tax_rate, 
-ROUND(SAFE_DIVIDE(SAFE_CAST(total_discounts AS FLOAT64), SAFE_CAST(total_line_items_price AS FLOAT64))) AS discount_rate, 
-ROUND(SAFE_DIVIDE(SAFE_CAST(line_items__price AS FLOAT64), SAFE_CAST(total_line_items_price AS FLOAT64))) AS shipping_rate, 
-ROUND(SAFE_DIVIDE(SAFE_CAST(line_items__price AS FLOAT64), SAFE_CAST(total_line_items_price AS FLOAT64))) AS refund_rate
-FROM `leslunes-prep.orders.unique_orders_it` as orders #`les-lunes-data-269915.import_it.unique_orders` as orders
-LEFT JOIN (SELECT 
-#Columns from unique_items view
-SAFE_CAST(line_items__id AS STRING) AS line_items__id,
-SAFE_CAST(line_items__variant_id AS STRING) AS line_items__variant_id,
-SAFE_CAST(line_items__title AS STRING) AS line_items__title,
-SAFE_CAST(line_items__quantity AS INT64) AS line_items__quantity,
-SAFE_CAST(line_items__sku AS STRING) AS line_items__sku,
-SAFE_CAST(line_items__variant_title AS STRING) AS line_items__variant_title,
-SAFE_CAST(line_items__vendor AS STRING) AS line_items__vendor,
-SAFE_CAST(line_items__fulfillment_service AS STRING) AS line_items__fulfillment_service,
-SAFE_CAST(line_items__product_id AS STRING) AS line_items__product_id,
-SAFE_CAST(line_items__requires_shipping AS BOOL) AS line_items__requires_shipping,
-SAFE_CAST(line_items__taxable AS BOOL) AS line_items__taxable,
-SAFE_CAST(line_items__gift_card AS BOOL) AS line_items__gift_card,
-SAFE_CAST(line_items__name AS STRING) AS line_items__name,
-SAFE_CAST(line_items__variant_inventory_management AS STRING) AS line_items__variant_inventory_management,
-SAFE_CAST(line_items__product_exists AS BOOL) AS line_items__product_exists,
-SAFE_CAST(line_items__fulfillable_quantity AS INT64) AS line_items__fulfillable_quantity,
-SAFE_CAST(line_items__grams AS INT64) AS line_items__grams,
-SAFE_CAST(line_items__price AS FLOAT64) AS line_items__price,
-SAFE_CAST(line_items__total_discount AS FLOAT64) AS line_items__total_discount,
-SAFE_CAST(line_items__fulfillment_status AS STRING) AS line_items__fulfillment_status,
-SAFE_CAST(line_items__admin_graphql_api_id AS STRING) AS line_items__admin_graphql_api_id,
-SAFE_CAST(line_items__price_set__shop_money__amount AS FLOAT64) AS line_items__price_set__shop_money__amount,
-SAFE_CAST(line_items__price_set__shop_money__currency_code AS STRING) AS line_items__price_set__shop_money__currency_code,
-SAFE_CAST(line_items__price_set__presentment_money__amount AS FLOAT64) AS line_items__price_set__presentment_money__amount,
-SAFE_CAST(line_items__price_set__presentment_money__currency_code AS STRING) AS line_items__price_set__presentment_money__currency_code,
-SAFE_CAST(line_items__total_discount_set__shop_money__amount AS FLOAT64) AS line_items__total_discount_set__shop_money__amount,
-SAFE_CAST(line_items__total_discount_set__shop_money__currency_code AS STRING) AS line_items__total_discount_set__shop_money__currency_code,
-SAFE_CAST(line_items__total_discount_set__presentment_money__amount AS FLOAT64) AS line_items__total_discount_set__presentment_money__amount,
-SAFE_CAST(line_items__total_discount_set__presentment_money__currency_code AS STRING) AS line_items__total_discount_set__presentment_money__currency_code,
-SAFE_CAST(line_items__origin_location__id AS STRING) AS line_items__origin_location__id,
-SAFE_CAST(line_items__origin_location__country_code AS STRING) AS line_items__origin_location__country_code,
-SAFE_CAST(line_items__origin_location__province_code AS STRING) AS line_items__origin_location__province_code,
-SAFE_CAST(line_items__origin_location__name AS STRING) AS line_items__origin_location__name,
-SAFE_CAST(line_items__origin_location__address1 AS STRING) AS line_items__origin_location__address1,
-SAFE_CAST(line_items__origin_location__address2 AS STRING) AS line_items__origin_location__address2,
-SAFE_CAST(line_items__origin_location__city AS STRING) AS line_items__origin_location__city,
-SAFE_CAST(line_items__origin_location__zip AS STRING) AS line_items__origin_location__zip,
-#SAFE_CAST(line_items__destination_location__id AS STRING) AS line_items__destination_location__id,
-#SAFE_CAST(line_items__destination_location__country_code AS STRING) AS line_items__destination_location__country_code,
-#SAFE_CAST(line_items__destination_location__province_code AS STRING) AS line_items__destination_location__province_code,
-#SAFE_CAST(line_items__destination_location__name AS STRING) AS line_items__destination_location__name,
-#SAFE_CAST(line_items__destination_location__address1 AS STRING) AS line_items__destination_location__address1,
-#SAFE_CAST(line_items__destination_location__address2 AS STRING) AS line_items__destination_location__address2,
-#SAFE_CAST(line_items__destination_location__city AS STRING) AS line_items__destination_location__city,
-#SAFE_CAST(line_items__destination_location__zip AS STRING) AS line_items__destination_location__zip,
-SAFE_CAST(order__id AS STRING) AS order__id,
-#SAFE_CAST(line_items_properties__name AS STRING) AS line_items_properties__name,
-#SAFE_CAST(line_items_properties__value AS STRING) AS line_items_properties__value,
-SAFE_CAST(item_type AS STRING) AS item_type
-FROM `leslunes-prep.items.unique_items_it`) as items ON CAST(orders.id AS STRING) = items.order__id
-LEFT JOIN ( 
-#Columns from unique_refunds
-SELECT order_id, SUM(refund_amount) AS refund_amount FROM 
-(SELECT ROW_NUMBER() OVER (PARTITION BY id, kind ORDER BY _sdc_batched_at DESC) AS rn, id, order_id, status, kind, amount AS refund_amount, _sdc_extracted_at 
-FROM `leslunes-raw.shopify_it.transactions` WHERE status='success' AND kind='refund') AS A 
-WHERE rn=1
-GROUP BY order_id) AS refunds ON CAST(orders.id AS STRING) = CAST(refunds.order_id AS STRING)
+  id AS shopify_order_id,
+  DATE(created_at) AS created_at_utc,
+  
+  li.value.sku AS shopify_sku,
+  li.value.title AS shopify_product_title,
+  li.value.variant_title AS shopify_variant_title ,	
+  li.value.name AS shopify_product_name,
+
+  li.value.price AS item_price_original,
+  li.value.quantity AS ordered_quantity,
+
+  SAFE_CAST(da.value.amount_set.shop_money.amount AS NUMERIC) AS item_discount,
+  tx.value.rate AS tax_rate,
+  tx.value.price AS tax_amount,
+  tx.value.title AS tax_type,
+
+  li.value.total_discount_set.shop_money.currency_code,
+  li.value.taxable,
+  li.value.gift_card,
+  li.value.requires_shipping,
+  li.value.vendor,
+
+  note,
+  number,
+  order_number,
+  name AS shopify_order_identifier,
+  li.value.id AS line_item_id,
+  li.value.product_id AS shopify_product_id,
+  li.value.variant_id AS shopify_variant_id
+
+FROM line_items,
+UNNEST(li.value.tax_lines)As tx,
+UNNEST(li.value.discount_allocations) AS da
+WHERE row_number = 1
+ORDER BY id
+
