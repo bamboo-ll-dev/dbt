a55b2cffdd858e21803f054084d9c3f1ef322dc1
Sebastian <sebastian@leslunes.de>
2021-06-30 11:29:00 +0200

    add customers dataset

---
+++ models/staging/customers/new_customers_daily_de.sql
@@ -0,0 +1,26 @@
+SELECT
+  SAFE_CAST(sale_date AS DATE) as date,
+  COUNT(DISTINCT email) AS new_customers,
+FROM (
+  SELECT
+    *,
+    first_sale=sale_date AS new_customer
+  FROM (
+    SELECT
+      email,
+      created_at AS sale_date
+    FROM leslunes-prep.orders.unique_orders_de
+    WHERE source_name='web'
+    GROUP BY email, sale_date) AS A
+  LEFT JOIN (
+    SELECT email AS email2, MIN(created_at) AS first_sale
+    FROM leslunes-prep.orders.unique_orders_de
+    WHERE source_name='web'
+    GROUP BY email) AS B
+  ON  A.email=B.email2) AS A
+  WHERE new_customer = true
+GROUP BY
+  date,
+  new_customer
+ORDER BY
+  date
