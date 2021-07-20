93a1b04d97b3cfcb1d17fd9f65e2078cd9de54ce
Sebastian <sebastian@leslunes.de>
2021-07-20 11:56:40 +0200

    add freegift and coupon models

---
+++ models/staging/coupons/stg_coupon_codes_fr.sql
@@ -0,0 +1,11 @@
+
+SELECT distinct
+  o.id AS transaction_id,
+  dapp.value.value_type AS code_value_type,
+  dapp.value.type AS code_type,	
+  dapp.value.value AS code_value,
+  dapp.value.code AS code
+FROM  
+  {{source('shopify_fr', 'orders')}} o,
+  UNNEST(o.discount_applications) AS dapp 
+WHERE dapp.value.type != "script"
---
+++ models/staging/coupons/stg_freegift_codes_fr.sql
@@ -0,0 +1,12 @@
+/*
+* freegifts are implemented by script in shopify
+*/
+SELECT distinct
+  li.value.id AS line_item_id,
+  dapp.value.title AS fg_title,
+  dapp.value.value AS fg_value
+FROM {{source('shopify_fr', 'orders')}} o,
+ UNNEST(line_items) AS li, 
+ UNNEST(o.discount_applications) as dapp,
+ UNNEST(li.value.properties) livp
+WHERE dapp.value.type = "script" AND livp.value.name	= "ll_fg" AND livp.value.value	= "true"
